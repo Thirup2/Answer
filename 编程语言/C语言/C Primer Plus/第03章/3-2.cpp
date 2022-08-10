92069266eda052a0ff3bff912647dead8491a2cd
Thirup2 <246946332@qq.com>
2022-08-10 15:30:52 +0800

    C Primer Plus ( 第三章 C++部分重做 )

---
+++ 编程语言/C语言/C Primer Plus/第03章/3-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "请输入一个ASCII码: " << endl;
+    short i;
+    cin >> i;
+    char ch = i;
+    cout << ch << endl;
+
+    return 0;
+}
