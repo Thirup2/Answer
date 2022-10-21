75f400501d9731f971811252abb7beba340a5e80
Thirup2 <246946332@qq.com>
2022-10-22 00:01:15 +0800

    大话数据结构
    单链表--添加

--- 数据结构/《大话数据结构》/3 链表/1 单链表/LinkList.h
+++ 数据结构/《大话数据结构》/3 链表/1 单链表/LinkList.h
@@ -5,183 +5,154 @@
 /* 包含头文件 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /* 数据类型定义 */
 struct items
 {
     int x;
     int y;
-};                              // 元素项
-typedef struct items ElemType;  // 元素类型
+};
+typedef struct items ElemType;
+
 struct node
 {
     ElemType data;
     struct node *next;
-};                              // 结点定义
-typedef struct node Node;       // 结点类型
-typedef Node *PNode;            // 结点指针
+};
+typedef struct node Node;
+typedef Node *PNode;
+
 struct list
 {
-    PNode start;                // 头指针
-    int length;                 // 链表长度
-};                              // 链表定义
-typedef struct list LinkList;   // 链表类型
+    PNode head;
+    int length;
+};
+typedef struct list LinkList;
+typedef LinkList *List;
 
 /* 数据元素处理函数原型 */
 
 /*
-** 操作: 比较两个数据元素是否相同
-** 参数: 两个数据元素
+** 操作: 比较两个数据元素
+** 参数: 两个数据元素指针
 ** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出;
-** 2. 若两元素相同则返回1;
-** 3. 若不同返回0
+** 1. 若相同返回true
+** 2. 否则返回false
 */
-int CompareElems(ElemType const *elem1, ElemType const *elem2);
+bool CompareElem(ElemType *elem1, ElemType *elem2);
 
 /*
-** 操作: 将一个数据元素赋值给另一个数组元素
-** 参数: 第一个参数为被赋值的元素, 第二个参数为赋值的元素
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出
+** 操作: 给数据元素赋值
+** 参数:
+** 1. 被赋值的元素
+** 2. 用于赋值的元素
 */
-void ChangeElem(ElemType *data, ElemType const *elem);
+void CopyElem(ElemType *elem, ElemType *data);
 
 /*
-** 操作: 打印一个数据元素的内容
-** 参数: 一个待打印的数据元素
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出
+** 操作: 打印一个数据元素
+** 参数: 一个数据元素
 */
-void ShowElem(ElemType const *elem);
+void PrintElem(ElemType *elem);
 
 /*
-** 操作: 获取一个元素
+** 操作: 读取一个数据元素
 ** 参数: 一个元素指针
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出
 */
-void GetElem(ElemType *elem);
+void ScanElem(ElemType *elem);
 
-/* 单链表接口函数元素 */
+/* 链表接口函数原型 */
 
 /*
-** 操作: 判断一个链表是否为空
-** 参数: 一个链表指针
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出;
-** 2. 若链表为空, 返回1;
-** 3. 否则返回0
+** 操作: 初始化操作, 建立一个空链表(包含一个头结点)
+** 返回值: 链表指针类型
 */
-int ListEmpty(LinkList const *list);
+List InitList(void);
+
+/*
+** 操作: 判断链表是否为空
+** 参数: 链表指针
+** 返回值:
+** 1. 若链表为空返回true
+** 2. 否则返回false
+*/
+bool ListEmpty(List list);
+
+/*
+** 操作: 将链表清空
+** 参数: 链表指针
+*/
+void ClearList(List list);
+
+/*
+** 操作: 销毁链表
+** 参数: 链表指针
+*/
+void DestroyList(List list);
+
+/*
+** 操作: 返回链表中第i个位置的元素
+** 参数:
+** 1. 链表指针
+** 2. 位置i
+** 3. 用于返回元素的指针
+** 返回值:
+** 1. 若链表为空, 返回-1
+** 2. 若位置不合理, 返回1
+** 3. 否则执行操作返回0
+*/
+int GetElem(List list, int i, ElemType *elem);
+
+/*
+** 操作: 定位元素在链表中的位置
+** 参数:
+** 1. 链表指针
+** 2. 待定位的元素指针
+** 返回值:
+** 1. 若链表为空则返回-1
+** 2. 若无该元素返回0
+** 3. 否则返回该元素的位序
+*/
+int LocateElem(List list, ElemType *elem);
+
+/*
+** 操作: 插入元素到链表
+** 参数:
+** 1. 链表指针
+** 2. 插入位序
+** 3. 插入元素
+** 返回值:
+** 1. 若链表已满返回-1
+** 2. 若插入位序不合理返回1
+** 3. 否则执行操作返回0
+*/
+int ListInsert(List list, int i, ElemType *elem);
+
+/*
+** 操作: 删除链表中第i个位置的元素
+** 参数:
+** 1. 链表指针
+** 2. 删除位序
+** 3. 用于返回的元素
+** 返回值:
+** 1. 若链表为空返回-1
+** 2. 若删除位序不合理返回1
+** 3. 否则执行操作返回0
+*/
+int ListDelete(List list, int i, ElemType *elem);
 
 /*
 ** 操作: 返回链表的元素个数
-** 参数: 一个链表指针
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出;
-** 2. 否则返回链表元素个数
+** 参数: 链表指针
+** 返回值: 链表元素个数
 */
-int ListLength(LinkList const *list);
+int ListLength(List list);
 
 /*
-** 操作: 打印链表所有元素的内容
-** 参数: 一个链表指针
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出;
-** 2. 链表未初始化返回-1;
-** 3. 否则执行操作返回0
+** 操作: 打印链表元素
+** 参数: 链表指针
 */
-int ListTraverse(LinkList const *list);
-
-/*
-** 操作: 初始化链表, 创建一个头结点并使头指针指向头结点, 表长设置为0
-** 参数: 一个链表指针
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出;
-** 2. 头结点创建失败返回-1;
-** 3. 否则执行操作返回0
-*/
-int InitList(LinkList *list);
-
-/*
-** 操作: 清除链表内容, 只保留头结点, 表长置为0
-** 参数: 一个链表指针
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出;
-** 2. 若链表未初始化则返回-1;
-** 3. 否则执行操作返回0
-*/
-int ClearList(LinkList *list);
-
-/*
-** 操作: 插入元素, 表长加1
-** 参数: 一个链表指针, 待插入的位序, 待插入的元素指针
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出;
-** 2. 链表未初始化返回-1
-** 3. 若插入位序不合理返回1;
-** 4. 若链表已满返回-2;
-** 5. 否则执行操作返回0
-*/
-int ListInsert(LinkList *list, int i, ElemType const *elem);
-
-/*
-** 操作: 删除元素, 表长减1, 并将被删除的元素赋值给第三参数
-** 参数: 一个链表指针, 待删除的位序, 待返回的元素指针
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出;
-** 2. 若链表未初始化返回-1;
-** 3. 若链表无元素返回-2;
-** 4. 若删除位序不合理返回1;
-** 5. 否则执行操作返回0
-*/
-int ListDelete(LinkList *list, int i, ElemType *elem);
-
-/*
-** 操作: 修改元素内容, 并将被修改元素赋值给第四参数
-** 参数: 一个链表指针, 待修改的位序, 用于修改的元素, 被修改的元素指针
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出;
-** 2. 若链表未初始化返回-1;
-** 3. 若链表无元素返回-2;
-** 4. 若位序不合理返回1;
-** 5. 否则执行操作返回0
-*/
-int ListChange(LinkList *list, int i, ElemType const *data, ElemType *elem);
-
-/*
-** 操作: 定位某个元素在链表中的位序
-** 参数: 一个链表指针, 待查找的元素指针
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出;
-** 2. 若链表未初始化返回-1;
-** 3. 若链表无元素返回-2;
-** 4. 若未找到该值, 返回0;
-** 5. 否则返回该值的位序
-*/
-int LocateElem(LinkList const *list, ElemType const *elem);
-
-/*
-** 操作: 将链表中某个位序的元素返回给元素指针
-** 参数: 一个链表指针, 待查找的位序, 待返回的元素指针
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出;
-** 2. 若链表未初始化返回-1;
-** 3. 若链表为空返回-2;
-** 4. 若位序不合理返回1;
-** 5. 否则执行操作返回0
-*/
-int ListSearch(LinkList const *list, int i, ElemType *elem);
-
-/*
-** 操作: 销毁链表, 包括头结点
-** 参数: 一个链表指针
-** 返回值:
-** 1. 若参数指针错误则打印错误信息并退出;
-** 2. 若链表未初始化返回-1;
-** 3. 否则执行操作返回0
-*/
-int DestroyList(LinkList *list);
+void PrintList(List list);
 
 #endif
---
+++ 数据结构/《大话数据结构》/3 链表/1 单链表/Main.c
@@ -0,0 +1,145 @@
+/* 链表测试程序源文件Main.c, 和LinkList.c一起编译 */
+
+/* 包含头文件 */
+#include "LinkList.h"
+
+/* 链表测试模块函数原型 */
+void ShowList(List list);
+void Insert(List list);
+void Delete(List list);
+void Locate(List list);
+void Finde(List list);
+void EatChar(void);
+
+/* 主函数 */
+int main(void)
+{
+    List list = InitList();
+    ShowList(list);
+    Insert(list);
+    Delete(list);
+    Locate(list);
+    Finde(list);
+    ClearList(list);
+    ShowList(list);
+    DestroyList(list);
+
+    return 0;
+}
+
+/* 链表测试模块函数定义 */
+void ShowList(List list)
+{
+    printf("当前链表:\n");
+    printf("    >> 是否为空: ");
+    printf("%s\n", ListEmpty(list) ? "是" : "否");
+    printf("    >> 元素个数: ");
+    printf("%d\n", ListLength(list));
+    printf("    >> 元素内容: ");
+    PrintList(list);
+}
+
+void Insert(List list)
+{
+    printf("开始插入元素...\n");
+    printf("请输入待插入位序(按q退出): ");
+    int i;
+    int r;
+    ElemType elem;
+    while (scanf("%d", &i) == 1) {
+        EatChar();
+        ScanElem(&elem);
+        EatChar();
+        r = ListInsert(list, i, &elem);
+        if (r == -1) {
+            printf("申请内存失败!\n");
+            break;
+        } else if (r == 1) {
+            printf("插入位序不合理, 请重新输入(按q退出): ");
+        } else if (r == 0) {
+            printf("插入成功!\n");
+            printf("继续插入请输入插入位序, 否则请按q: ");
+        }
+    }
+    EatChar();
+    ShowList(list);
+}
+
+void Delete(List list)
+{
+    printf("开始删除元素...\n");
+    printf("请输入待删除位序(退出请按q): ");
+    int i;
+    int r;
+    ElemType elem;
+    while (scanf("%d", &i) == 1) {
+        EatChar();
+        r = ListDelete(list, i, &elem);
+        if (r == -1) {
+            printf("链表为空!\n");
+            break;
+        } else if (r == 1) {
+            printf("删除位序不合理, 请重新输入(按q退出): ");
+        } else if (r == 0) {
+            printf("删除成功! 被删除的元素是: (%d, %d)\n", elem.x, elem.y);
+            printf("继续删除请输入删除位序, 否则请按q: ");
+        }
+    }
+    EatChar();
+    ShowList(list);
+}
+
+void Locate(List list)
+{
+    printf("开始定位元素...\n");
+    ElemType elem;
+    int i;
+    printf("请输入待查找的元素(按\"x, y\"的格式): ");
+    while (scanf("%d, %d", &elem.x, &elem.y) == 2) {
+        EatChar();
+        i = LocateElem(list, &elem);
+        if (i == -1) {
+            printf("链表为空!\n");
+            break;
+        } else if (i == 0) {
+            printf("该元素不存在!\n");
+            printf("请重新输入元素(按\"x, y\"的格式): ");
+        } else {
+            printf("元素(%d, %d)在链表中第%d个元素位置\n", elem.x, elem.y, i);
+            printf("继续查找请输入待查找的元素(按\"x, y\"的格式): ");
+        }
+    }
+    EatChar();
+    ShowList(list);
+}
+
+void Finde(List list)
+{
+    printf("开始查找元素...\n");
+    int i;
+    ElemType elem;
+    int r;
+    printf("请输入要查找的位序(按q退出): ");
+    while (scanf("%d", &i) == 1) {
+        EatChar();
+        r = GetElem(list, i, &elem);
+        if (r == -1) {
+            printf("链表为空!\n");
+            break;
+        } else if (r == 1) {
+            printf("插入位置不合理, 请重新输入(按q退出): ");
+        } else if (r == 0) {
+            printf("位序为%d位置的元素为: (%d, %d)\n", i, elem.x, elem.y);
+            printf("继续查找请输入位序(按q退出): ");
+        }
+    }
+    EatChar();
+    ShowList(list);
+}
+
+void EatChar(void)
+{
+    while (getchar() != '\n') {
+        continue;
+    }
+}
