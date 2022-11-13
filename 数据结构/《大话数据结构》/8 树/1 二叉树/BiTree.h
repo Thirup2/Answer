36ff61f1b07f7080ef1189764b252d7bdc033821
Thirup2 <246946332@qq.com>
2022-11-13 23:59:30 +0800

    暂存

--- 数据结构/《大话数据结构》/8 树/1 二叉树/BiTree.h
+++ 数据结构/《大话数据结构》/8 树/1 二叉树/BiTree.h
@@ -15,222 +15,72 @@ struct items
 };
 typedef struct items ElemType;
 
-struct pos
+struct position
 {
-    int level;
-    int order;
+    int level;  // 层数
+    int order;  // 位置
 };
-typedef struct pos Pos;
-
-struct status
-{
-    ElemType elem;
-    Pos position;
-};
-typedef struct status Data;
+typedef struct position Position;
+typedef Position *PtrPosition;
 
 struct node
 {
-    Data data;
-    struct node *leftchild;
-    struct node *rightchild;
+    ElemType data;
+    Position pos;
+    struct node *lchild;
+    struct node *rchild;
     struct node *parent;
-    struct node *rightbrother;
+    struct node *rbrother;
 };
 typedef struct node Node;
-typedef Node *PNode;
+typedef Node *PtrNode;
 
 struct tree
 {
-    PNode root;
-    int length;
+    PtrNode root;
+    Position status;        // 保存深度与最大叶子位置
+    int length;             // 有效元素的个数
 };
-typedef struct tree BiTree;
-typedef BiTree *PtrBiTree;
-
-typedef int BRANCH;
-#define RIGHTBRANCH 1
-#define LEFTBRANCH 2
-
-/* 数据元素处理函数原型 */
-
-/*
-** 操作: 修改结点数据的值
-** 参数:
-** 1. 待修改的元素指针
-** 2. 用于修改的元素指针
-*/
-void ChangeElem(ElemType *elem1, ElemType *elem2);
-
-/*
-** 操作: 比较两个结点的元素值
-** 参数: 两个用于比较的元素指针
-** 返回值:
-** 1. 若两元素相等, 返回true
-** 2. 否则返回false
-*/
-bool CompareElem(ElemType *elem1, ElemType *elem2);
+typedef struct tree Tree;
+typedef Tree *PtrTree;
 
 /* 二叉树接口函数原型 */
 
 /*
-** 操作: 构造空树
-** 返回值: 返回一个空树的指针
+** 操作：构造空树
+** 返回值：树指针
 */
-PtrBiTree InitTree(void);
+PtrTree InitTree(void);
 
 /*
-** 操作: 销毁树
-** 参数: 待销毁的树
+** 操作：销毁树
+** 参数：树指针
 */
-void DestroyTree(PtrBiTree tree);
+void DestroyTree(PtrTree tree);
 
 /*
-** 操作: 按definition给出的定义构造树
-** 参数: 一个树模板definition
-** 返回值: 一个构造完成的树指针
+** 操作：按definition给出的定义构造树
+** 参数：树定义
+** 返回值：树指针
 */
-PtrBiTree CreatTree(PtrBiTree definition);
+PtrTree CreatTree(PtrTree definition);
 
 /*
-** 操作: 清空树
-** 参数: 树指针
+** 操作：清空树
+** 参数：树指针
 */
-void ClearTree(PtrBiTree tree);
+void ClearTree(PtrTree tree);
 
 /*
-** 操作: 判断树是否为空
-** 参数: 树指针
-** 返回值:
-** 1. 若树为空, 返回true
+** 操作：判断是否空树
+** 参数：树指针
+** 返回值：
+** 1. 若为空树则返回true
 ** 2. 否则返回false
 */
-bool TreeEmpty(PtrBiTree tree);
+bool TreeEmpty(PtrTree tree);
 
 /*
-** 操作: 返回树的深度
-** 参数: 树指针
-** 返回值: 树的深度
-*/
-int TreeDepth(PtrBiTree tree);
-
-/*
-** 操作: 返回树的根结点
-** 参数: 树指针
-** 返回值:
-** 1. 若树为空, 返回NULL
-** 2. 否则返回树的根结点数据指针
-*/
-ElemType *Root(PtrBiTree tree);
-
-/*
-** 操作: 返回指定结点的值
-** 参数:
-** 1. 树指针
-** 2. 指定结点
-** 返回值:
-** 1. 若该结点不存在, 返回NULL
-** 2. 返回该结点的数据指针
-*/
-ElemType *Value(PtrBiTree tree, Pos position);
-
-/*
-** 操作: 给指定结点赋值
-** 1. 若该结点存在, 则改变原有值
-** 2. 若该结点不存在, 若该结点允许创建, 则创建结点, 并赋值
-** 参数:
-** 1. 树指针
-** 2. 指定结点指针
-** 3. 元素指针
-** 返回值:
-** 1. 若该结点不允许创建, 返回-1
-** 2. 否则创建结点并赋值或直接赋值返回0
-*/
-int Assign(PtrBiTree tree, Pos position, ElemType *elem);
-
-/*
-** 操作: 返回指定结点的双亲结点的元素值
-** 参数:
-** 1. 树指针
-** 2. 指定结点位置
-** 返回值:
-** 1. 若指定结点为根结点或指定结点不存在, 返回NULL
-** 2. 否则返回指定结点双亲元素指针
-*/
-ElemType *Parent(PtrBiTree tree, Pos position);
-
-/*
-** 操作: 返回指定结点的左孩子结点元素
-** 参数:
-** 1. 树指针
-** 2. 指定结点位置
-** 返回值:
-** 1. 若指定结点或指定结点的左孩子结点不存在, 返回NULL
-** 2. 否则返回指定结点左孩子元素指针
-*/
-ElemType *LeftChild(PtrBiTree tree, Pos position);
-
-/*
-** 操作: 返回指定结点右兄弟结点元素
-** 参数:
-** 1. 树指针
-** 2. 指定结点位置
-** 返回值:
-** 1. 若指定结点或指定结点的右兄弟结点不存在, 返回NULL
-** 2. 否则返回指定结点右兄弟元素指针
-*/
-ElemType *RightBrother(PtrBiTree tree, Pos position);
-
-/*
-** 操作: 在树中指定结点的指定分支插入指定子树
-** 参数:
-** 1. 树指针
-** 2. 指定结点
-** 3. 指定分支
-** 4. 指定子树
-** 返回值:
-** 1. 若指定结点不存在, 返回-1
-** 2. 若指定结点的指定分支已有值, 返回-2
-** 3. 否则执行操作返回0
-*/
-int InsertChild(PtrBiTree tree, Pos position, BRANCH branch, PtrBiTree subtree);
-
-/*
-** 操作: 删除树中指定结点的指定分支的子树
-** 参数:
-** 1. 树指针
-** 2. 指定结点
-** 3. 指定分支
-** 4. 用于返回分支子树的指针
-** 返回值:
-** 1. 若指定结点不存在, 返回-1
-** 2. 若指定结点的指定分支不存在, 返回-2
-** 3. 否则执行操作返回0
-*/
-int DeleteChild(PtrBiTree tree, Pos position, BRANCH branch, PtrBiTree subtree);
-
-/*
-** 操作: 前序遍历树
-** 参数:
-** 1. 树指针
-** 2. 数据处理函数
-*/
-void PreOrderTraverse(PtrBiTree tree, void (*function)(PNode node, ElemType *elem));
-
-/*
-** 操作: 中序遍历树
-** 参数:
-** 1. 树指针
-** 2. 数据处理函数
-*/
-void InOrderTraverse(PtrBiTree tree, void (*function)(PNode node));
-
-/*
-** 操作: 后序遍历树
-** 参数:
-** 1. 树指针
-** 2. 数据处理函数
-*/
-void PostOrderTraverse(PtrBiTree tree, void (*function)(PNode node));
+** 操作：返回树的深度 */
 
 #endif
--- 数据结构/《大话数据结构》/8 树/1 二叉树/main.c
+++
@@ -1,4 +0,0 @@
-/* 二叉树驱动程序源文件main.c, 和BiTree.c一起编译 */
-
-/* 包含头文件 */
-#include "BiTree.h"
