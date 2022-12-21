70c1893142b9bcede6a06e2f7ed90391e1f3e9fc
Thirup2 <246946332@qq.com>
2022-12-21 16:26:01 +0800

    更新

--- 数据结构/《大话数据结构》/8 树/1 二叉树/BiTree.c
+++ 数据结构/《大话数据结构》/8 树/1 二叉树/BiTree.c
@@ -9,20 +9,48 @@
 ** 操作：构造空树
 ** 返回值：树指针
 */
-PtrTree InitTree(void);
+PtrTree InitTree(void)
+{
+    PtrTree tree = (PtrTree) malloc(sizeof(Tree));
+    if (!tree) {
+        exit(EXIT_FAILURE);
+    }
+    tree->root = NULL;
+    tree->status.level = 0;
+    tree->status.order = 0;
+    tree->length = 0;
+    return  tree;
+}
 
 /*
 ** 操作：销毁树
 ** 参数：树指针
 */
-void DestroyTree(PtrTree tree);
+void DestroyTree(PtrTree tree)
+{
+    ClearTree(tree);
+    free(tree);
+}
 
 /*
 ** 操作：按definition给出的定义构造树
 ** 参数：树定义
 ** 返回值：树指针
 */
-PtrTree CreatTree(PtrTree definition);
+PtrTree CreatTree(PtrTree definition)
+{
+    if (TreeEmpty(definition) == false) {
+        return NULL;
+    }
+    PtrTree tree = (PtrTree) malloc(sizeof(Tree));
+    if (!tree) {
+        exit(EXIT_FAILURE);
+    }
+    tree->length = definition->length;
+    tree->status.level = definition->status.level;
+    tree->status.order = definition->status.order;
+    // 复制每一个结点的所有内容（其中指针要相对转移为指向当前树对应位置的指针）
+}
 
 /*
 ** 操作：清空树
@@ -37,14 +65,23 @@ void ClearTree(PtrTree tree);
 ** 1. 若为空树则返回true
 ** 2. 否则返回false
 */
-bool TreeEmpty(PtrTree tree);
+bool TreeEmpty(PtrTree tree)
+{
+    if (tree->length == 0) {
+        return true;
+    }
+    return false;
+}
 
 /*
 ** 操作：返回树的深度
 ** 参数：树指针
 ** 返回值：数的深度
 */
-int TreeDepth(PtrTree tree);
+int TreeDepth(PtrTree tree)
+{
+    return tree->status.level;
+}
 
 /*
 ** 操作：返回树的根节点
@@ -55,7 +92,7 @@ int TreeDepth(PtrTree tree);
 */
 PtrNode Root(PtrTree tree);
 
-/* 
+/*
 ** 操作：返回指定结点的值
 ** 参数：
 ** 1. 树指针
@@ -66,7 +103,7 @@ PtrNode Root(PtrTree tree);
 */
 ElemType *Value(PtrTree tree, PtrPosition position);
 
-/* 
+/*
 ** 操作：给指定结点赋值
 ** 参数：
 ** 1. 树指针
@@ -124,7 +161,7 @@ PtrNode RightSibling(PtrTree tree, PtrPosition position);
 */
 int InsertChild(PtrTree tree, PtrPosition position, Leaf leaf, PtrTree subtree);
 
-/* 
+/*
 ** 操作：删除指定结点指定叶子的子树
 ** 参数：
 ** 1. 树指针
