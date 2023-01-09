17941e8b4432d9782046f8064eedf000421dfacb
Thirup2 <246946332@qq.com>
2023-01-09 14:19:57 +0800

    大话数据结构-更新

--- 数据结构/《大话数据结构》/0 Head/ElemType.h
+++ 数据结构/《大话数据结构》/0 Head/ElemType.h
@@ -1,9 +1,56 @@
-/* 数据元素接口实现 */
+/* 数据元素类型头文件 */
 #ifndef _ELEMTYPE_H_
 #define _ELEMTYPE_H_
 
 /* 包含头文件 */
-#include "ElemType_head.h"
+#include "Status.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/* 数据元素类型定义 */
+struct items
+{
+    int x;
+    int y;
+};
+typedef struct items ElemType;
+typedef ElemType *PtrElem;
+typedef const ElemType *cPtrElem;
+
+/* 数据元素函数接口 */
+
+/*
+** 操作：为数据元素赋值
+** 参数：
+** 1. 待赋值数据元素指针
+** 2. 赋值数据元素指针
+*/
+void AssignElem(PtrElem elem, cPtrElem data);
+
+/*
+** 操作：比较两个数据元素
+** 参数：待比较的两个数据元素指针
+** 返回值：
+** 1. 若两个数据元素相同返回true
+** 2. 否则返回false
+*/
+bool CompareElems(cPtrElem elem1, cPtrElem elem2);
+
+/*
+** 操作：打印一个元素内容
+** 参数：一个元素指针
+*/
+void PrintElem(cPtrElem elem);
+
+/*
+** 操作：读取一个元素内容
+** 参数：待读取的元素指针
+** 返回值：
+** 1. 若scanf返回值与待读取值个数相同返回true
+** 2. 否则返回false
+*/
+bool ReadElem(PtrElem elem);
 
 /* 接口实现 */
 
